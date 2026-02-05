9558c4af9ae24262c39208cc4ae5bd74096275e6
Philippe Gantzer <p.gantzer@icredd.hokudai.ac.jp>
2026-02-05 17:27:32 +0900

    logger debug

--- analysis/api/utils/optimizer.py
+++ analysis/api/utils/optimizer.py
@@ -200,7 +200,6 @@ def get_model(data):
         raise ValueError("No solution found. Try increasing iterations number or change parameters.")
     rebuild_trial = st.sort_values(by='score', ascending=False).iloc[0]
     logger.info("Required/done/best: {}/{}/{}".format(trials, len(st), rebuild_trial['trial']))
-    logger.info("BEST:", rebuild_trial)
     logger.info("TIME: {}".format(time.perf_counter() - start_time))
 
     scores_df = stats[rebuild_trial['trial']]['score'].iloc[0]
